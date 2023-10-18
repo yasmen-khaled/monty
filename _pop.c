8b15cc14c434343b879b065e7d6f5742cf5818cf
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-19 00:50:58 +0300

    Update _pop.c

--- _pop.c
+++ _pop.c
@@ -1,13 +1,21 @@
-void pint(stack_t **stack, unsigned int line_no)
+#include "monty.h"
+
+
+
+void pop(stack_t **stack, unsigned int line_no)
 {
+	stack_t *h;
+
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", line_no);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_no);
 		fclose(bus.file);
 		free(bus.content);
 		freemem(*stack);
 		exit(EXIT_FAILURE);
 	}
-	printf("%d\n", (*stack)->n);
+	h = *stack;
+	*stack = h->next;
+	free(h);
 
 }
