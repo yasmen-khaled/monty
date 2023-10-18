16bf28f25b307a07aae568dea775a2b889cae1af
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-19 01:06:24 +0300

    Update _pint.c

--- _pint.c
+++ _pint.c
@@ -2,14 +2,12 @@
 
 void pint(stack_t **stack, unsigned int line_no)
 {
-	if (*stack == NULL)
-	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", line_no);
-		fclose(bus.file);
-		free(bus.content);
-		freemem(*stack);
-		exit(EXIT_FAILURE);
-	}
-	printf("%d\n", (*stack)->n);
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "L%u: can't pint, stack empty\n", line_no);
+        exit(EXIT_FAILURE);
+    }
 
+    printf("%d\n", (*stack)->n);
 }
+
