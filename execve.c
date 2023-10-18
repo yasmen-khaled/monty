de2fe130adc63d24423f7934b7c0bd0ef45d5986
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-19 01:46:22 +0300

    Update execve.c

--- execve.c
+++ execve.c
@@ -1,22 +1,20 @@
-#include "monty.h"
-
 int execute(char *content, stack_t **stack, unsigned int line_no, FILE *file)
 {
-    instruction_t instructions[] = {
-        {"push", push_function}, {"pall", pall_function}, {"pint", pint_function},
-        {"pop", pop_function}, {"swap", swap_function}, {"add", add_function},
-        {"nop", nop_function}, {NULL, NULL}
+    instruction_t opst[] = {
+        {"push", push}, {"pall", pall}, {"pint", pint},
+        {"pop", pop}, {"swap", swap}, {"add", add},
+        {"nop", nop}, {NULL, NULL}
     };
 
     char *op = strtok(content, " \n\t");
     if (op && op[0] == '#')
         return 0;
 
-    for (unsigned int i = 0; instructions[i].opcode; i++)
+    for (unsigned int i = 0; opst[i].opcode; i++)
     {
-        if (strcmp(op, instructions[i].opcode) == 0)
+        if (strcmp(op, opst[i].opcode) == 0)
         {
-            instructions[i].function(stack, line_no);
+            opst[i].f(stack, line_no);
             return 0;
         }
     }
