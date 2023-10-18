1a63accadfd37d88d24f66d2e7cc5d5f3aaf781c
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-19 00:50:15 +0300

    Create _pop.c

---
+++ _pop.c
@@ -0,0 +1,13 @@
+void pint(stack_t **stack, unsigned int line_no)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_no);
+		fclose(bus.file);
+		free(bus.content);
+		freemem(*stack);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+
+}
