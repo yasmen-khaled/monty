63b0354c443cb3bb022d5c00694aea8ca35801f2
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-19 01:38:02 +0300

    Update execve.c

--- execve.c
+++ execve.c
@@ -32,6 +32,6 @@ int execute(char *content, stack_t **stack, unsigned int line_no, FILE *file)
     fprintf(stderr, "L%d: unknown instruction %s\n", line_no, op);
     fclose(file);
     free(content);
-    freem(*stack);
+    memo(*stack);
     exit(EXIT_FAILURE);
 }
