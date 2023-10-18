8aedda72f72c742892ba5e01a94c4a454fe2681d
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-19 00:47:46 +0300

    Update function_push_pall_pint.c

--- function_push_pall_pint.c
+++ function_push_pall_pint.c
@@ -1,40 +1,5 @@
 #include "monty.h"
-/**
- * push - add node to the stack
- * @stack: stack head
- * @line_no: line_number
- * Return: no return
- */
-void push(stack_t **stack, unsigned int line_no)
-{
-	int n, j = 0, flag = 0;
 
-	if (bus.arg)
-	{
-		if (bus.arg[0] == '-')
-			j++;
-		for (; bus.arg[j] != '\0'; j++)
-		{
-			if (bus.arg[j] > 57 || bus.arg[j] < 48)
-				flag = 1; }
-		if (flag == 1)
-		{ fprintf(stderr, "L%d: usage: push integer\n", line_no);
-			fclose(bus.file);
-			free(bus.content);
-			freemem(*stack);
-			exit(EXIT_FAILURE); }}
-	else
-	{ fprintf(stderr, "L%d: usage: push integer\n", line_no);
-		fclose(bus.file);
-		free(bus.content);
-		freemem(*stack);
-		exit(EXIT_FAILURE); }
-	n = atoi(bus.arg);
-	if (bus.lifi == 0)
-		addnode(stack, n);
-	else
-		addqueue(stack, n);
-}
 /**
  * pall - prints the stack
  * @stack: stack head
