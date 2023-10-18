e356ad46d96a31fac7d3875ff6e9cb806d15ccaa
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-19 00:51:57 +0300

    Update and rename function_push_pall_pint.c to _swap.c

---
+++ _swap.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+
+void swap(stack_t **stack, unsigned int line_no)
+{
+	stack_t *h = *stack;
+	int len = 0;
+
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_no);
+		freemem(*stack);
+		exit(EXIT_FAILURE);
+	}
+	if (*stack)
+	{
+		stack_t *tmp;
+
+		tmp = (*stack)->next;
+		(*stack)->next = tmp->next;
+		(tmp->next)->prev = *stack;
+		tmp->next = *stack;
+		(*stack)->prev = tmp;
+		tmp->prev = NULL;
+		*stack = tmp;
+	}
+}
