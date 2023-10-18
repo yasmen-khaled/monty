88f102234cabd83d99ef4eeef206c8518a835f0b
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-19 01:15:26 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,12 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 bus_t bus = {NULL, NULL, NULL, 0};
-/**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
+
 int main(int argc, char *argv[])
 {
 	char *content;
