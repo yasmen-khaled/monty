46edfcf26d93a3e510e44f3146d3cd8b4d38d33f
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-19 01:03:13 +0300

    Update _pall.c

--- _pall.c
+++ _pall.c
@@ -2,15 +2,14 @@
 
 void pall(stack_t **stack, unsigned int line_no)
 {
-	stack_t *h;
-	(void)line_no;
+    (void)line_no;
 
-	h = *stack;
-	if (h == NULL)
-		return;
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
+    stack_t *current = *stack;
+
+    while (current != NULL)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
 }
+
