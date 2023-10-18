a184241b1f9f3679a2fa843bd80043120b096e00
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-19 01:27:01 +0300

    Update execve.c

--- execve.c
+++ execve.c
@@ -1,33 +1,54 @@
 #include "monty.h"
-
- int execute(char *content, stack_t **stack, unsigned int line_no, FILE *file) {
+int execute(char *content, stack_t **stack, unsigned int line_no, FILE *file)
+{
     char *op = strtok(content, " \n\t");
     if (op && op[0] == '#')
         return 0;
 
     char *arg = strtok(NULL, " \n\t");
-    if (strcmp(op, "push") == 0) {
-        if (arg) {
-          
+    if (strcmp(op, "push") == 0)
+    {
+        if (arg)
+        {
             int value = atoi(arg);
             push(stack, value, line_no);
-        } else {
+        }
+        else
+        {
             fprintf(stderr, "L%d: missing argument for push\n", line_no);
             fclose(file);
             free(content);
             freemem(*stack);
             exit(EXIT_FAILURE);
         }
-    } else if (strcmp(op, "pall") == 0) {
+    }
+    else if (strcmp(op, "pall") == 0)
+    {
         pall(stack, line_no);
-    } else if (strcmp(op, "pint") == 0) {
+    }
+    else if (strcmp(op, "pint") == 0)
+    {
         pint(stack, line_no);
-    } else {
+    }
+    else if (strcmp(op, "pop") == 0)
+    {
+        pop(stack, line_no);
+    }
+    else if (strcmp(op, "swap") == 0)
+    {
+        swap(stack, line_no);
+    }
+    // Add other operations here...
+
+    else
+    {
         fprintf(stderr, "L%d: unknown instruction %s\n", line_no, op);
         fclose(file);
         free(content);
         freemem(*stack);
         exit(EXIT_FAILURE);
     }
+
     return 0;
 }
+
