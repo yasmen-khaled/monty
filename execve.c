ec68a51b0e0845ff7ebe6f303d1085c7b8ebb22b
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-20 00:16:05 +0300

    Update execve.c

--- execve.c
+++ execve.c
@@ -4,6 +4,7 @@
 
 int execute(char *content, stack_t **stack, unsigned int line_no, FILE *file)
 {
+    unsigned int i;
     instruction_t opst[] = {
         {"push", push}, {"pall", pall}, {"pint", pint},
         {"pop", pop}, {"swap", swap}, {"add", add},
@@ -16,7 +17,6 @@ int execute(char *content, stack_t **stack, unsigned int line_no, FILE *file)
 
     bus.arg = strtok(NULL, " \n\t");
 
-    unsigned int i;
     for (i = 0; opst[i].opcode != NULL; i++)
     {
         if (strcmp(op, opst[i].opcode) == 0)
