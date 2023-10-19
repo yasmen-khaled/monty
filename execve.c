3431f01b42fd7c31a4bcb5c3c3d36783a2fc7f3a
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-19 23:51:00 +0300

    Update execve.c

--- execve.c
+++ execve.c
@@ -4,28 +4,34 @@
 
 int execute(char *content, stack_t **stack, unsigned int line_no, FILE *file)
 {
-    instruction_t *opst = get_instructions();
+    instruction_t opst[] = {
+        {"push", push}, {"pall", pall}, {"pint", pint},
+        {"pop", pop}, {"swap", swap}, {"add", add},
+        {"nop", nop}, {NULL, NULL}
+    };
 
-    char *op = strtok(content, " \n\t");
-    if (op && op[0] == '#')
-        return 0;
+   unsigned int i = 0;
+	char *op;
 
-    bus.arg = strtok(NULL, " \n\t");
-
-    instruction_t *current = opst;
-    while (current != NULL)
-    {
-        if (strcmp(op, current->opcode) == 0)
-        {
-            current->f(stack, line_no);
-            return 0;
-        }
-        current = current->next;
-    }
-
-    fprintf(stderr, "L%d: unknown instruction %s\n", line_no, op);
-    fclose(file);
-    free(content);
-    memo(*stack);
-    exit(EXIT_FAILURE);
+	op = strtok(content, " \n\t");
+	if (op && op[0] == '#')
+		return (0);
+	bus.arg = strtok(NULL, " \n\t");
+	while (opst[i].opcode && op)
+	{
+		if (strcmp(op, opst[i].opcode) == 0)
+		{	opst[i].f(stack, line_no);
+			return (0);
+		}
+		i++;
+	}
+	if (op && opst[i].opcode == NULL)
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_no, op);
+		fclose(file);
+		free(content);
+		memo(*stack);
+		exit(EXIT_FAILURE);
+	}
+	return (1);
 }
