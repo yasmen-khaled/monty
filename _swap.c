00c224910dba3deb48126aafe19e2c867bba31d1
yasmen <yasmen.khaled2000@gmail.com>
2023-10-20 00:36:10 +0300

    DONE

--- _swap.c
+++ _swap.c
@@ -1,16 +1,24 @@
 #include "monty.h"
+
+/**
+ * swap - aps the top two elements of the stack.
+ *@stack:pointer
+ *@line_no: number
+ *
+ */
+
 void swap(stack_t **stack, unsigned int line_no)
 {
-    int tmp;
+	int tmp;
 
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%d: can't swap, stack too short\n", line_no);
-        memo(*stack);
-        exit(EXIT_FAILURE);
-    }
+	if (*stack == NULL || (*stack)->next == NULL)
+{
+	fprintf(stderr, "L%d: can't swap, stack too short\n", line_no);
+	memo(*stack);
+	exit(EXIT_FAILURE);
+}
 
-    tmp = (*stack)->n;
-    (*stack)->n = (*stack)->next->n;
-    (*stack)->next->n = tmp;
+	tmp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = tmp;
 }
