fdd3087dadbca7c0d13e7fa714ad03112c9bf99d
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-19 20:29:58 +0300

    Update _push.c

--- _push.c
+++ _push.c
@@ -2,32 +2,32 @@
 
 void push(stack_t **stack, unsigned int line_no)
 {
-	int n, j = 0, flag = 0;
+    char *endptr;
+    long int n;
 
-	if (bus.arg)
-	{
-		if (bus.arg[0] == '-')
-			j++;
-		for (; bus.arg[j] != '\0'; j++)
-		{
-			if (bus.arg[j] > 57 || bus.arg[j] < 48)
-				flag = 1; }
-		if (flag == 1)
-		{ fprintf(stderr, "L%d: usage: push integer\n", line_no);
-			fclose(bus.file);
-			free(bus.content);
-			memo(*stack);
-			exit(EXIT_FAILURE); }}
-	else
-	{ fprintf(stderr, "L%d: usage: push integer\n", line_no);
-		fclose(bus.file);
-		free(bus.content);
-		memo(*stack);
-		exit(EXIT_FAILURE); }
-	n = atoi(bus.arg);
-	if (bus.lifi == 0)
-		_addnode(stack, n);
-	else
-		_addqueue(stack, n);
+    if (bus.arg == NULL)
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", line_no);
+        fclose(bus.file);
+        free(bus.content);
+        memo(*stack);
+        exit(EXIT_FAILURE);
+    }
+
+    n = strtol(bus.arg, &endptr, 10);
+    if (*endptr != '\0')
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", line_no);
+        fclose(bus.file);
+        free(bus.content);
+        memo(*stack);
+        exit(EXIT_FAILURE);
+    }
+
+    if (bus.lifi == 0)
+        _addnode(stack, (int)n);
+    else
+        _addqueue(stack, (int)n);
 }
+
 
