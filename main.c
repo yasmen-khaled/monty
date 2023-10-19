15832520ae6c7179f3b45df2be91915de4e84e80
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-20 00:17:54 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,43 +1,34 @@
 #define _GNU_SOURCE
 #include "monty.h"
 
-
 bus_t bus = {NULL, NULL, NULL, 0};
 
 int main(int argc, char *argv[])
 {
-	char *content;
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int line_no = 0;
+    char content[1024];
+    FILE *file;
+    stack_t *stack = NULL;
+    unsigned int line_no = 0;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (read_line > 0)
-	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		line_no++;
-		if (read_line > 0)
-		{
-			execute(content, &stack, line_no, file);
-		}
-		free(content);
-	}
-	memo(stack);
-	fclose(file);
-return (0);
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+    file = fopen(argv[1], "r");
+    bus.file = file;
+    if (!file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    while (fgets(content, sizeof(content), file) != NULL)
+    {
+        bus.content = content;
+        line_no++;
+        execute(content, &stack, line_no, file);
+    }
+    memo(stack);
+    fclose(file);
+    return 0;
 }
