da183564e6b305218134861dd17a00c0ddb10647
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-20 00:50:59 +0300

    Update _add.c

--- _add.c
+++ _add.c
@@ -2,10 +2,16 @@
 
 void add(stack_t **stack, unsigned int line_no)
 {
-	int sum = (*stack)->n + ((*stack)->prev)->n;
-	(void)line_no;
-	*stack = (*stack)->next;
-	(*stack)->n = sum;
-	free((*stack)->prev);
-	(*stack)->prev = NULL;
+    if (*stack == NULL || (*stack)->prev == NULL)
+    {
+        fprintf(stderr, "L%d: can't add, stack too short\n", line_no);
+        memo(*stack);
+        exit(EXIT_FAILURE);
+    }
+
+    int sum = (*stack)->n + (*stack)->prev->n;
+    *stack = (*stack)->next;
+    (*stack)->n = sum;
+    free((*stack)->prev);
+    (*stack)->prev = NULL;
 }
