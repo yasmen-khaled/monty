7767c15fa3985d026d3e8f547668b2376f1b4a52
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-19 17:06:56 +0300

    Update execve.c

--- execve.c
+++ execve.c
@@ -9,22 +9,28 @@ int execute(char *content, stack_t **stack, unsigned int line_no, FILE *file)
         {"nop", nop}, {NULL, NULL}
     };
 
-    char *op = strtok(content, " \n\t");
-    if (op && op[0] == '#')
-        return 0;
+   unsigned int i = 0;
+	char *op;
 
-    for (unsigned int i = 0; opst[i].opcode; i++)
-    {
-        if (strcmp(op, opst[i].opcode) == 0)
-        {
-            opst[i].f(stack, line_no);
-            return 0;
-        }
-    }
-
-    fprintf(stderr, "L%d: unknown instruction %s\n", line_no, op);
-    fclose(file);
-    free(content);
-    memo(*stack);
-    exit(EXIT_FAILURE);
+	op = strtok(content, " \n\t");
+	if (op && op[0] == '#')
+		return (0);
+	bus.arg = strtok(NULL, " \n\t");
+	while (opst[i].opcode && op)
+	{
+		if (strcmp(op, opst[i].opcode) == 0)
+		{	opst[i].f(stack, line_no);
+			return (0);
+		}
+		i++;
+	}
+	if (op && opst[i].opcode == NULL)
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_no, op);
+		fclose(file);
+		free(content);
+		memo(*stack);
+		exit(EXIT_FAILURE);
+	}
+	return (1);
 }
