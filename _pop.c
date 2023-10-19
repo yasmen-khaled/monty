4fb9acac7e34d9e51afd005ad3e2e88a79d872b7
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-10-19 16:41:51 +0300

    Update _pop.c

--- _pop.c
+++ _pop.c
@@ -2,13 +2,15 @@
 
 void pop(stack_t **stack, unsigned int line_no)
 {
+    stack_t *temp;
+
     if (*stack == NULL)
     {
         fprintf(stderr, "L%d: can't pop an empty stack\n", line_no);
         exit(EXIT_FAILURE);
     }
 
-    stack_t *temp = *stack;
+    temp = *stack;
     *stack = (*stack)->next;
     free(temp);
 }
